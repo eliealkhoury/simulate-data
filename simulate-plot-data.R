f3be732dfbf5ebaf4433aa31a16565db35b364db
Elie Al Khoury <lc22-0442@lclark.edu>
2024-06-25 23:09:01 +0000

    Initial Commit

---
+++ simulate-plot-data.R
@@ -0,0 +1,12 @@
+#Simulate and plot data
+#Elie AL Khoury
+#eliea@lclark.edu
+#2024-06-25
+
+
+#simulate predictor variable
+x <- rnorm(n = 100)
+#simulate response variable with noise
+y <- 2 * x  + rnorm(n = 100, sd = 0.2)
+#plot the data
+plot(x = x, y = y)
